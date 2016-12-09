f1b0412d9d26f70064b6c84ba3efa8f31f0adaed
nunoms <nunos@LENOVO-NUNOS>
2016-12-09 09:28:50 +0000

    added missing BuggyAPI project

---
+++ SpeakerResources/VS Projects/BuggyApp/BuggyAPI/BuggyAPIClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuggyAPI
+{
+    public class BuggyAPIClient
+    {
+        public void LoadInfo(bool useExternalAPI)
+        {
+            if (useExternalAPI)
+            {
+                Random r = new Random();
+                var delay = r.Next(28000, 35000);
+                System.Threading.Thread.Sleep(delay);
+            }
+            else
+            {
+                Random r = new Random();
+                var delay = r.Next(0, 1000);
+                System.Threading.Thread.Sleep(delay);
+            }
+        }
+    }
+}
